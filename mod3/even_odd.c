53d28d3e0500605ba6d85513a3957cfca9c604e2
Christian Willener <monster.domo975@gmail.com>
2019-01-24 09:59:55 -0700

    2 inputs

--- mod3/even_odd.c
+++ mod3/even_odd.c
@@ -8,7 +8,7 @@
  *        Version:  1.0
  *        Created:  01/24/2019 09:36:39 AM
  *       Revision:  none
- *       Compiler:  gcc even_odd.c -o ecen_odd.out
+ *       Compiler:  gcc even_odd.c -o even_odd.out
  *
  *         Author:  Christian Willener (), christianwillener@mail.weber.edu
  *   Organization:  WSU
@@ -24,16 +24,20 @@
 // Main Function
 int main()
 {
-    int num;
+    int num1;
+    int num2;
 
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-
-    if(num % 2 == 0)
+    printf("Enter an even and odd number: ");
+    scanf("%d %d", &num1, &num2);
+    if((num1 + num2) % 2 != 0)
     {
-        printf("%d is even\n", num);
+        printf("%d and %d are even and odd\n", num1, num2);
     }
-    printf("%d is odd\n", num);
+    else
+    {
+        printf("%d and %d are not even and odd\n", num1, num2);
+    }
+    printf("Bye bye\n");
 
     return 0;
 }
