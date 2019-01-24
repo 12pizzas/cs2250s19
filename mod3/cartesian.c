a31c3db086f45fd3886b8ee07d769bf1ea33ac5a
Christian Willener <monster.domo975@gmail.com>
2019-01-24 08:59:32 -0700

    Added algebra

--- mod3/cartesian.c
+++ mod3/cartesian.c
@@ -20,7 +20,7 @@
 #include <math.h>
 
 // Constants
-
+const double PI = 3.14159;
 // Function Prototypes
 
 // Main Function
@@ -28,18 +28,25 @@ int main()
 {
     double radius;
     double angleDegrees;
+    double x_pos;
+    double y_pos;
+    double angleRadians;
     // 1) Capture user input for radius and theta angle
     printf("Enter radius: ");
     scanf("%lf", &radius);
     printf("Enter angle in degrees: ");
     scanf("%lf", &angleDegrees);
-    printf("%lf %lf\n", radius, angleDegrees);
+    //printf("%lf %lf\n", radius, angleDegrees);
+
     // 2) Convert the angle to radians
+    angleRadians = (angleDegrees*PI/180);
 
     // 3) Calculate cartesian coordiantes
+    x_pos = radius*cos(angleRadians);
+    y_pos = radius*sin(angleRadians);
 
     // 4) Print results
-    
+    printf("%lf %lf", x_pos, y_pos);
     
 
     return 0;
